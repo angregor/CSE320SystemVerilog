3f0b3814cddc61f1386f1853e9782f9ff3c85c65
Andrew Gregory <angregor@asu.edu>
2015-11-29 12:16:38 -0700

    addresscounter 2nd draft, fixed some logic errors

--- addressCounter.sv
+++ addressCounter.sv
@@ -1,5 +1,5 @@
 module addressCounter(input logic clock, input logic reset, input logic desDone, input logic sDone,
-                      input logic CNT_EN, output logic[15:0] address);
+                      output logic[15:0] address);
 
 always@(posedge clock) begin
   case(reset)
@@ -7,14 +7,12 @@ always@(posedge clock) begin
       address = 0;
       end
     1'b0: begin
-      case(CNT_EN)
-        1'b1: begin
-          address = address + 1;
-        end
-        1'b0: begin
-          address = address;
-        end
-      endcase
+      if(sDone or desDone) begin
+        address = address + 1;
+      end
+      else begin
+        address = address;
+      end
     end
   endcase
 end
