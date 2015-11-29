748be6501eb6b9cdc8fcd14ab7eb94eecdb7999d
Andrew Gregory <angregor@asu.edu>
2015-11-29 12:10:35 -0700

    addressCounter first draft

--- addressCounter.sv
+++ addressCounter.sv
@@ -1,4 +1,22 @@
 module addressCounter(input logic clock, input logic reset, input logic desDone, input logic sDone,
-                      output logic[15:0] address);
+                      input logic CNT_EN, output logic[15:0] address);
+
+always@(posedge clock) begin
+  case(reset)
+    1'b1: begin
+      address = 0;
+      end
+    1'b0: begin
+      case(CNT_EN)
+        1'b1: begin
+          address = address + 1;
+        end
+        1'b0: begin
+          address = address;
+        end
+      endcase
+    end
+  endcase
+end
 
 endmodule
