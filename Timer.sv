a6db6c3c9f19f36dcb97ca328bcdd0d9d1f1fbde
Andrew Gregory <angregor@asu.edu>
2015-11-29 12:09:42 -0700

    timer first draft

--- Timer.sv
+++ Timer.sv
@@ -1,3 +1,33 @@
 module Timer(input logic clock, input logic reset, input logic isEnabled, output logic secondMarker);
 
+logic currentCount = 0;
+
+always@(posedge clock) begin
+  case(reset)
+    1'b0: begin
+      case(isEnabled)
+        1'b0: begin
+          currentCount = currentCount
+          secondMarker = 1'b0;
+        end
+        1'b1: begin
+          case(currentCount)
+            21'b111101000010010000000: begin
+              secondMarker = 1'b1;
+            end
+            default: begin
+              secondMarker = 1'b0;
+              currentCount = currentCount + 1;
+            end
+          endcase
+        end
+      endcase
+    end
+    1'b1: begin
+      secondMarker = 1'b0;
+      currentCount = 0;
+    end
+  endcase
+end
+
 endmodule
