3777d63f4a907a32d12ef4e20d32ebd06a09157f
Andrew Gregory <angregor@asu.edu>
2015-11-29 12:46:56 -0700

    fixed synchronizer module definition

--- Synchronizer.sv
+++ Synchronizer.sv
@@ -1,6 +1,4 @@
-module Syncrhonizer(input logic clock, input logic reset, input logic resetButton,
-                    input logic recordButton, input logic playButton, input logic clipRecordNum,
-                    input logic clipPlayNum, output logic resetBSync, output logic recordBSync,
-                    output logic playBSync, output logic clipRSync, output logic clipPSync);
+module Syncrhonizer(input logic clock, input logic reset, input logic resetButton, input logic clipNum, input logic playOrRecord, input logic ActionButton,
+  output logic ActionSync, output logic ClipNumSync, output logic PlayOrRecordSync, output logic resetButtonSync);
 
 endmodule
