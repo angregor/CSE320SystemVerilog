a3ff8ccbfff29f160788aee50d62c6e41b1d30ef
Andrew Gregory <angregor@asu.edu>
2015-11-29 10:52:46 -0700

    clock scalar first draft, created module definition for address counter

---
+++ addressCounter.sv
@@ -0,0 +1,4 @@
+module addressCounter(input logic clock, input logic reset, input logic desDone, input logic sDone,
+                      output logic[15:0] address);
+
+endmodule
--- clockScalar.sv
+++ clockScalar.sv
@@ -1,3 +1,21 @@
-module clockScalar(input logic clock, input logic reset, output logic scaledClock);
+module clockScalar(input logic clk, input logic reset, output logic scaled_clk);
+
+parameter n = 26;
+reg [n-1:0] count;
+
+always@(posedge clk) begin
+    if(reset)
+        count <= 0;
+    else
+        count <= count + 1;
+end
+
+always@(posedge clk) begin
+    scaled_clk = count[0];
+    if(reset) begin
+        scaled_clk = 1;
+        end
+
+end
 
 endmodule
