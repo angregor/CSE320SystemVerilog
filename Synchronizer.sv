9514b4ee0769412dff24bdaf70039bbe008336f9
Andrew Gregory <angregor@asu.edu>
2015-12-02 18:05:28 -0700

    new synchronizer

--- Synchronizer.sv
+++ Synchronizer.sv
@@ -1,41 +1,55 @@
-module Syncrhonizer(input logic clock, input logic reset, input logic resetButton, input logic clipNumPlay, input logic clipNumRecord, input logic play, input logic record,
-  output logic ClipNumPlaySync, output logic ClipNumRecordSync, output logic PlaySync, output logic RecordSync, output logic resetButtonSync);
+//{{ Section below this comment is automatically maintained
+//   and may be overwritten
+//{module {Edge_Detector}}
+module Edge_Detector (
+	input clk,
+	input play,
+	input record,
+	input clipNumPlay,
+	input clipNumRecord,
+	input reset,
+	output playSync,
+	output recordSync,
+	output clipPlaySync,
+	output clipRecordSync,
+	output resetSync);
+//}} End of automatically maintained section
 
+// -- Enter your statements here -- //
+reg playreg, play_1, play_2;
+reg recordreg, record_1, record_2;
+reg clipPlayreg, clipP_1, clipP_2;
+reg clipRecreg, clipR_1, clipR_2;
+reg resetreg, reset_1, reset_2;
 
-reg playReg;
-reg recordReg;
-reg resetButtonReg;
-reg clipNumPlayReg;
-reg clipNumRecordReg;
+always@(posedge clk)
+begin
+	playreg <= play;
+	play_1 <= playreg;
+	play_2 <= play_1;
 
-always@(play or record or resetButton or clipNumPlay or clipNumRecord) begin
-  if(play)
-    playReg = 1;
-  if(record)
-    recordReg = 1;
-  if(resetButton)
-    resetButtonReg = 1;
-  if(clipNumPlay)
-    clipNumPlayReg = 1;
-  if(clipNumRecord)
-    clipNumRecordReg = 1;
+	recordreg <= record;
+	record_1 <= recordreg;
+	record_2 <= record_1;
+
+	clipPlayreg <= clipNumPlay;
+	clipP_1 <= clipPlayreg;
+	clipP_2 <= clipP_1;
+
+	clipRecreg <= clipNumRecord;
+	clipR_1 <= clipRecreg;
+	clipR_2 <= clipR_1;
+
+	resetreg <= reset;
+	reset_1 <= resetreg;
+	reset_2 <= reset_1;
 end
 
-always@(posedge clock) begin
-  PlaySync = playReg;
-  playReg = 0;
+assign playSync = play_1 & ~play_2;
+assign recordSync = record_1 & ~record_2;
+assign clipPlaySync = clipP_1 & ~clipP_2;
+assign clipRecordSync = clipR_1 & ~clipR_2;
+assign resetSync = reset_1 & ~reset_2;
 
-  RecordSync = recordReg;
-  recordReg = 0;
-
-  resetButtonSync = resetButtonReg;
-  resetButtonReg = 0;
-
-  ClipNumPlaySync = clipNumPlayReg;
-  clipNumPlayReg = 0;
-
-  ClipNumRecordSync = clipNumRecordReg;
-  clipNumRecordReg = 0;
-end
 
 endmodule
